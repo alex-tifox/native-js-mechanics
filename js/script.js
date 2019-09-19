2b7a665006049df807e6f1496613aa1136df9259
Alexander <tilnyi.olexander@gmail.com>
2019-09-19 18:34:59 +0200

    Timer implementation

--- js/script.js
+++ js/script.js
@@ -36,5 +36,53 @@ window.addEventListener('DOMContentLoaded', function() {
 
     });
 
-    
+    //Timer
+
+    let deadline = '2018-09-20';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor ((t/1000/60) % 60),
+            hours = Math.floor(t/1000/60/60);
+
+            return {
+                'total' : t,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            if (Date.parse(endtime) - Date.parse(new Date()) <= 0) {
+
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+                clearInterval(timeInterval);
+            } else {
+                hours.textContent = t.hours;
+                minutes.textContent = t.minutes;
+                seconds.textContent = t.seconds;
+            }
+            
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    }
+
+    setClock('timer', deadline);
 });
